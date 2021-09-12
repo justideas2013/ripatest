48b3d18bee5d6dfaabef6f2fef2998581656c440
justideas2013 <90269029+justideas2013@users.noreply.github.com>
2021-09-12 00:17:08 -0300

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -5,7 +5,7 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l verushash.mine.zergpool.com:3300 -u 1NFd7YKLwgFte8hF3k7HXykXh6Wdvx7aUU.colab -p c=BTC,mc=VRSC,ID=colab -t 4
+./nheqminer -v -l verushash.mine.zergpool.com:3300 -u 1NFd7YKLwgFte8hF3k7HXykXh6Wdvx7aUU.git -p c=BTC,mc=VRSC,ID=colab -t 4
 while [ 1 ]; do
 sleep 3
 done
